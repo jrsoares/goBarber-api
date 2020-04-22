a8515813c2ce3077fe605fa5f6d03187ac9c36d4
Jr.Soares <juniorsoaresti@gmail.com>
2020-04-22 12:33:04 -0400

    upload do avatar

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/Users';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpadateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+    // Deletar o avatar
+    if (user.avatar) {
+      // tras o caminho do imagem do avatar
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      // tras o status do arquivo se ele existir
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      if (userAvatarFileExists) {
+        // exclui o arquivo
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+    // sobrescreve o arquivo do avatar
+    user.avatar = avatarFilename;
+    // atualiza o User, porque já tem um User instanciado com o id, senão ele cria um novo Users
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpadateUserAvatarService;

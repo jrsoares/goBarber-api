e89daf9a4135f6af08061b564e84e26f71c6de21
Jr.Soares <juniorsoaresti@gmail.com>
2020-06-18 03:57:40 -0400

    Implementando os controllers

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,22 +1,9 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
-import CreateSessionService from '@modules/users/services/CreateSessionService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const sessionUser = container.resolve(CreateSessionService);
-
-  const { user, token } = await sessionUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;

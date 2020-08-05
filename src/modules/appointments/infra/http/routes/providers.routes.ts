a83d1b1a415a090b4d3207764b2fca43d3f951a1
Júnior Soares <jrsoares@MacBook-Air-de-Junior.local>
2020-08-05 09:36:56 -0400

    Listagem de prestadores

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+
+const providersRouter = Router();
+const providersController = new ProvidersController();
+
+// middleware
+providersRouter.use(ensureAuthenticated);
+
+/* providersRouter.get('/', async (request, response) => {
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+}); */
+
+providersRouter.get('/', providersController.index);
+
+export default providersRouter;
